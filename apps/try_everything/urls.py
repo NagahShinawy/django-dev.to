e0514562289029147c216a5aa63fb5cc5b6551df
Nagah <E-n.Shinawy@lean.sa>
2021-09-07 16:59:26 +0300

    adding simple swagger

--- apps/try_everything/urls.py
+++ apps/try_everything/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
+from rest_framework_swagger.views import get_swagger_view
+
 from .views import home, to_excel
+schema_view = get_swagger_view(title='Timeline API')
 
 
 urlpatterns = [
     path('', home, name="home"),
     path('export/', to_excel, name="to_excel"),
+    path('api/', schema_view, name="api"),
 ]
